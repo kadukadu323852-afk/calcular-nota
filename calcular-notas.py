3cab91a8ec6f81fd9371d35f8dbf8bb0f667a03d
cadu <kadukadu323852@gmail.com>
2026-02-24 14:30:44 -0300

    usando função e fazendo tratamento de erros

--- calcular-notas.py
+++ calcular-notas.py
@@ -1,17 +1,25 @@
-nota1 = -1
-while nota1 <0 or nota1 >10:
-  nota1 = float(input("informe a primeira nota: "))
-nota2 = -1
-while nota2 <0 or nota2 >10:
-  nota2 = float(input("informe a segunda nota: "))
-nota3 = -1
-while nota3 <0 or nota3 >10:
-  nota3 = float(input("informe a terceira nota: "))
+
+
+def perguntar_nota(mensagem):
+    while True:
+        try:
+            nota = float(input(mensagem))
+            if 0 <= nota <= 10:
+                return nota
+            else:
+                print("Nota deve estar entre 0 e 10. Tente novamente.")
+        except ValueError:
+            print("Entrada inválida. Por favor, insira um número válido.")
+
+nota1 = perguntar_nota("informe a primeira nota: ")
+nota2 = perguntar_nota("informe a segunda nota: ")
+nota3 = perguntar_nota("informe a terceira nota: ")
+
 nota = (nota1 + nota2 + nota3) / 3
 print(f"nota {nota:.2f}")
 if nota <= 3.0:
   print("reprovado")
-elif nota > 3.0 and nota <= 5.9:
+elif nota > 3.0 and nota <6:
  print("recuperação")
 else:
   print("aprovado")
